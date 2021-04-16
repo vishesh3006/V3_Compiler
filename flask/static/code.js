bbded7ef6e5aa1659b1646ce9d2654ead1ede0a8
vishesh,gupta <vishesh1999gupta@gmail.com>
2021-04-17 00:38:02 +0530

    js changes in flask

--- flask/static/code.js
+++ flask/static/code.js
@@ -1,11 +1,30 @@
+
+// alert("ssss");
 document.getElementById("content").innerHTML =
   localStorage["text"] || "This text is automatically saved every second :) "; // default text
 
 setInterval(function() {
   // fuction that is saving the innerHTML of the div
+  
   localStorage["text"] = document.getElementById("content").innerHTML; // content div
-  console.log(document.getElementById("content").innerText)
+  console.log("sss");
+  // console.log(document.getElementById("content").innerText)
 }, 1000);
 
 
 
+$(document).ready( () => {
+  $("button").on("click", () => {
+    alert("Clicked")
+    var data = $("#content").innerText;
+    var lang = $( "#language option:selected" ).text();
+     $.ajax({
+      url: "/compile",
+      type: "POST",
+      data: {data: data, lang: lang},
+      success: function() {
+        console.log("Yes")
+      },
+     });   
+  });
+});
